300c19f0145d11338215fe30aa527e23354ce24c
Templarisman <62556201+Templarisman@users.noreply.github.com>
2025-06-26 14:23:33 +0900

    1423

--- application/clinic_server/main.py
+++ application/clinic_server/main.py
@@ -1,14 +1,16 @@
+
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from fastapi_base_server.application.clinic_server.routers import clinic
-
+from .routers import clinic
 from template.base.template_context import TemplateContext
 from template.base.template_type import TemplateType
 from template.clinic.clinic_template_impl import ClinicTemplateImpl
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    TemplateContext.add_template(TemplateType.CLINIC, ClinicTemplateImpl())
+    clinic_template_instance = ClinicTemplateImpl()
+    clinic_template_instance.init(config=None)
+    TemplateContext.add_template(TemplateType.CLINIC, clinic_template_instance)
     yield
 
 app = FastAPI(lifespan=lifespan)
@@ -17,4 +19,5 @@ app.include_router(clinic.router, prefix="/clinic", tags=["clinic"])
 
 @app.get("/")
 def root():
-    return {"message": "Clinic Server is running"} 
+    return {"message": "Clinic Server is running"}
+
